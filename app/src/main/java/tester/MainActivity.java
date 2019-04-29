ecffb573a880509b68e586f92cfdcfa0d8578399
vinod <vinodghai713@gmail.com>
2019-04-29 16:35:36 +0500

    master/demo app packaging changed

--- app/src/main/java/tester/MainActivity.java
+++ app/src/main/java/tester/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.vendvinodkumar.customcamera;
+package tester;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import com.example.vendvinodkumar.customcamera.R;
 
 import net.titan.camera.cameras.CameraProperties;
 import net.titan.camera.interfaces.CaptureResult;
